b01572735844f3c51916ead4811c9cbc0eccd103
Ken McGrady <ken.mcgrady@gmail.com>
2016-06-23 14:22:09 -0700

    Added Auth

--- server/server.js
+++ server/server.js
@@ -6,8 +6,10 @@ const port = process.env.PORT || 8000;
 
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const session = require('cookie-session');
 
-const authRoutes = require('./routes/auth');
+const userRoutes = require('./routes/users');
 const authorRoutes = require('./routes/authors');
 const bookRoutes = require('./routes/books');
 
@@ -18,9 +20,16 @@ app.disable('x-powered-by');
 app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(session({
+  keys: [
+    'key1',//process.env.SESSION_KEY1,
+    'key2',//process.env.SESSION_KEY2
+  ]
+}));
 
 app.use(express.static(path.join('..', 'client')));
-app.use('/auth', authRoutes);
+app.use('/users', userRoutes);
 app.use('/authors', authorRoutes);
 app.use('/books', bookRoutes);
 
@@ -31,7 +40,7 @@ app.use((_req, _res, next) => {
 });
 
 app.use((err, _req, res, _next) => {
-  res.status(err.status || 500).send('Error:', err);
+  res.status(err.status || 500).send(err);
 });
 
 app.listen(port, () => {
