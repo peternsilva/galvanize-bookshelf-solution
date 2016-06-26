732765c49a94e75017ef1baf35c5830b3ab60a0e
Ryan Sobol <contact@ryansobol.com>
2016-06-26 07:36:03 -0700

    Add tests for part1

--- server/knexfile.js
+++ server/knexfile.js
@@ -6,6 +6,11 @@ module.exports = {
     connection: 'postgres://localhost/bookshelf_dev'
   },
 
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/bookshelf_test'
+  },
+
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL
